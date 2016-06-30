69da74f7927e4afc0a75a4dbf47709c68a9a37b5
Zaid <Zaid@Zaid-PC>
2016-06-30 18:32:25 -0400

    change

---
+++ JavaDayOne/src/Conditionals/Switch.java
@@ -0,0 +1,27 @@
+package Conditionals;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		int orange =8;
+		String apple = "5";
+		
+		switch (apple){
+		
+		case ("5"):
+		
+			System.out.println("not so many apples");
+				break;
+		
+		default :
+			System.out.println("so many apples");
+				
+		}
+		
+
+			
+		}
+
+	}
+
---
+++ JavaDayOne/src/Conditionals/ifStatements.java
@@ -0,0 +1,29 @@
+package Conditionals;
+
+public class ifStatements {
+
+	public static void main(String[] args) {
+		
+		boolean yesNo;
+		int orange = 5;
+		int apple = 10;
+		int berry;
+		orange +=3;
+		
+		if (orange > 7 && apple >= 5){
+			System.out.println("He got 5 dollars");
+		}else if (apple == 5){
+			System.out.println("Amazing");
+		}else {
+			System.out.println ("broke");
+		}
+		
+		berry = (orange > apple) ? 5:3;
+				
+		System.out.println (berry);
+		yesNo = (1 >2) ? true  : false;
+			if (yesNo = true){
+	}
+		
+}
+}
